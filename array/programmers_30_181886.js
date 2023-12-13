929692b87a1bfbcb61b1e7eee45d74dca6925f22
Hyobin Choe <95601940+AtomicLiquors@users.noreply.github.com>
2023-12-13 23:30:45 +0900

    Create programmers_30_181886.js

---
+++ array/programmers_30_181886.js
@@ -0,0 +1,14 @@
+function solution(names) {
+    let answer = [];
+    let idx = 0;
+    let length = names.length;
+    
+    for(let name of names){
+        answer.push(names[idx]);
+        idx += 5;
+        
+        if(idx >= length)
+            break;
+    }
+    return answer;
+}
